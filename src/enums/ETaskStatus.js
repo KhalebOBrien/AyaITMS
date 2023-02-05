14bd06f88d5c308c36d195853baa2853078faf52
404: CTO_not_found <ebukauche52@gmail.com>
2023-02-05 03:29:03 +0100

    task model

---
+++ src/enums/ETaskStatus.js
@@ -0,0 +1,5 @@
+export const ETaskStatus = {
+  TODO: 'todo',
+  INPROGRESS: 'in_progress',
+  DONE: 'done',
+}
---
+++ src/models/Task.js
@@ -0,0 +1,51 @@
+import mongoose from 'mongoose'
+import { ETaskStatus } from '../enums/ETaskStatus'
+
+const taskSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Please enter a name.'],
+    },
+    description: {
+      type: String,
+    },
+    taskboard: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'taskboards',
+    },
+    creator: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'users',
+    },
+    status: {
+      type: String,
+      enum: ETaskStatus,
+      default: ETaskStatus.TODO,
+    },
+    position: {
+      type: Number,
+    },
+    deleted: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: {
+      createdAt: 'created_at',
+      updatedAt: 'updated_at',
+    },
+    toJSON: {
+      virtuals: true,
+    },
+  },
+)
+
+taskSchema.methods.toJSON = function () {
+  var obj = this.toObject()
+  delete obj.__v
+  return obj
+}
+
+export const Task = mongoose.model('tasks', taskSchema)
